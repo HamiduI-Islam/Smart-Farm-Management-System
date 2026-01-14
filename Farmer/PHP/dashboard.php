830f129f2b7d6e1e314462feb6f4238f3ca169b0
Shumaya <jahanshumaya@gmail.com>
2026-01-15 00:47:34 +0600

    db,login,register,dashboard added

---
+++ Farmer/PHP/dashboard.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Farmer Dashboard</title>
+    <link rel="stylesheet" href="../CSS/dasboard.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+
+    <main class="dashboard-content" id="dashboard-content">
+        <h1>Welcome>!</h1>
+        <p>Select a feature from the sidebar to see details here.</p>
+    </main>
+
+    <script>
+        
+        const links = document.querySelectorAll('.sidebar a');
+        const content = document.getElementById('dashboard-content');
+
+        links.forEach(link => {
+            link.addEventListener('click', function(e){
+                e.preventDefault(); 
+
+                let featureName = this.textContent.trim();
+                content.innerHTML = `
+                    <h1>${featureName}</h1>
+                    <p>This is the ${featureName} feature.</p>
+                `;
+            });
+        });
+    </script>
+</body>
+</html>
